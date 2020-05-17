0a3199e126385c3216010690b16c22dddd0a2cf3
dpamar <dpamar@gmail.com>
2020-05-17 12:15:19 +0200

    Update places.js

--- places.js
+++ places.js
@@ -1,3 +1,16 @@
 var places = [
+    [45.181022, 5.723881, 'Ouaf ouaf'],
+    [45.182227, 5.725662, 'Mais non pas la tefila'],
+    [45.183144, 5.726428, 'On passe entre les poteaux ?'],
+    [45.184392, 5.725576, 'La la la !'],
+    [45.185196, 5.727216, 'C est fermé !!!'],
+    [45.186407, 5.728396, 'Aaaah un lion !!!'],
+    [45.187, 5.727023, 'On arrête de courir !'],
+    [45.185523, 5.723053, 'Il est où le vélo en or ?'],
+    [45.184567, 5.723587, 'Demain demain...'],
+    [45.183225, 5.724231, 'Quand est-ce que papy et mamie reviennent ?'],
+    [45.183236, 5.723371, 'Non on ne va pas au cinéma ! En plus il est fermé '],
+    [45.181734, 5.724693, 'On achète du pain ?'],
+    [45.180747, 5.722373, 'Je sais pas du tout où ça peut bien être ...']
 ];
-
+//Resume game designer URL: https://dpamar.github.io/sortie1705/generator.html?places=W1s0NS4xODEwMjIsIDUuNzIzODgxLCAnT3VhZiBvdWFmJ10sWzQ1LjE4MjIyNywgNS43MjU2NjIsICdNYWlzIG5vbiBwYXMgbGEgdGVmaWxhJ10sWzQ1LjE4MzE0NCwgNS43MjY0MjgsICdPbiBwYXNzZSBlbnRyZSBsZXMgcG90ZWF1eCA/J10sWzQ1LjE4NDM5MiwgNS43MjU1NzYsICdMYSBsYSBsYSAhJ10sWzQ1LjE4NTE5NiwgNS43MjcyMTYsICdDIGVzdCBmZXJt6SAhISEnXSxbNDUuMTg2NDA3LCA1LjcyODM5NiwgJ0FhYWFoIHVuIGxpb24gISEhJ10sWzQ1LjE4NzAwMCwgNS43MjcwMjMsICdPbiBhcnLqdGUgZGUgY291cmlyICEnXSxbNDUuMTg1NTIzLCA1LjcyMzA1MywgJ0lsIGVzdCBv+SBsZSB26WxvIGVuIG9yID8nXSxbNDUuMTg0NTY3LCA1LjcyMzU4NywgJ0RlbWFpbiBkZW1haW4uLi4nXSxbNDUuMTgzMjI1LCA1LjcyNDIzMSwgJ1F1YW5kIGVzdC1jZSBxdWUgcGFweSBldCBtYW1pZSByZXZpZW5uZW50ID8nXSxbNDUuMTgzMjM2LCA1LjcyMzM3MSwgJ05vbiBvbiBuZSB2YSBwYXMgYXUgY2lu6W1hICEgRW4gcGx1cyBpbCBlc3QgZmVybekgJ10sWzQ1LjE4MTczNCwgNS43MjQ2OTMsICdPbiBhY2jodGUgZHUgcGFpbiA/J10sWzQ1LjE4MDc0NywgNS43MjIzNzMsICdKZSBzYWlzIHBhcyBkdSB0b3V0IG/5IOdhIHBldXQgYmllbiDqdHJlIC4uLiddXQ==
